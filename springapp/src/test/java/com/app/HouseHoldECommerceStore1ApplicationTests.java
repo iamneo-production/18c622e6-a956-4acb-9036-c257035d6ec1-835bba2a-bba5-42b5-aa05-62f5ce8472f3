f89c9c0b22edc2bcd3d7b48303a924928733b723
AswanthVR <v.r.aswanth111@gmail.com>
2023-10-03 19:16:53 +0000

    Added backend

--- springapp/src/test/java/com/app/HouseHoldECommerceStore1ApplicationTests.java
+++ springapp/src/test/java/com/app/HouseHoldECommerceStore1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springapp;
+package com.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringappApplicationTests {
+class HouseHoldECommerceStore1ApplicationTests {
 
 	@Test
 	void contextLoads() {
